065be7b2ae8e728560f15abbfdbec24429464753
alexandre-mace <contact.alexandremace@gmail.com>
2020-02-15 22:18:14 +0100

    Updates

---
+++ precache-manifest.6cda71546ac62365c8525625f3b3722c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5d9feb9480e49aa36df1c9e23b9802",
+    "url": "/action/index.html"
+  },
+  {
+    "revision": "8c5110e931c4345e10cf",
+    "url": "/action/static/css/2.c6435436.chunk.css"
+  },
+  {
+    "revision": "fe7bd463be98957d2250",
+    "url": "/action/static/css/main.6a46b6bd.chunk.css"
+  },
+  {
+    "revision": "8c5110e931c4345e10cf",
+    "url": "/action/static/js/2.2a2c8a07.chunk.js"
+  },
+  {
+    "revision": "751b1a643df72051cd189fd334235919",
+    "url": "/action/static/js/2.2a2c8a07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe7bd463be98957d2250",
+    "url": "/action/static/js/main.b334d882.chunk.js"
+  },
+  {
+    "revision": "e1acc1d0519d8f75ba8e",
+    "url": "/action/static/js/runtime-main.3a44de23.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/action/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/action/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/action/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/action/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/action/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/action/precache-manifest.91d18b3f305559f381412663025def44.js"
+  "/action/precache-manifest.6cda71546ac62365c8525625f3b3722c.js"
 );
 
 self.addEventListener('message', (event) => {
